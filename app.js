e817035b550f75ed8b6a1410fbfc17c1bc2280c7
Ethan Bruning <ebruning@gmail.com>
2015-07-01 10:07:14 -0700

    Base the app from the html5 sample

---
+++ app.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  var regionUls = {
+    "United States": "//mobiledemo.kofax.com/mobilesdk/api/mobileid?xCropImage=True",
+    "Canada": "//mobiledemo.kofax.com/mobilesdk/api/checkdeposit1_1?processimage=true",
+    "Africa": "//mobiledemo.kofax.com/mobilesdk/api/mobileid?xCropImage=True",
+    "Asia": "//mobiledemo.kofax.com/mobilesdk/api/checkdeposit1_1?processimage=true",
+    "Australia": "//mobiledemo.kofax.com/mobilesdk/api/mobileid?xCropImage=True",
+    "Europe": "//mobiledemo.kofax.com/mobilesdk/api/checkdeposit1_1?processimage=true",
+    "Latin America": "//mobiledemo.kofax.com/mobilesdk/api/mobileid?xCropImage=True",
+    "Singapore": "//mobiledemo.kofax.com/mobilesdk/api/checkdeposit1_1?processimage=true"
+  };
+
+  var select = document.getElementById('regionSelect');
+
+  select.innerHTML = Object.keys(demoUrls).map(function (data) {
+    return '<option value="' + demoUrls[data] + '">' + data + '</option>';
+  });
+}
